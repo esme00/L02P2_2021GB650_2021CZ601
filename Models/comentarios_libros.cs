dc4b7b876571c54946ce4d8f3538e24a933de1df
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-04-20 08:55:54 -0600

    Frank

--- Models/comentarios_libros.cs
+++ Models/comentarios_libros.cs
@@ -1,6 +1,14 @@
-﻿namespace L02P2_2021GB650_2021CZ601.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P2_2021GB650_2021CZ601.Models
 {
     public class comentarios_libros
     {
+        [Key]
+        public int id { get; set; }
+        public int id_libro { get; set; }
+        public string comentarios { get; set; }
+        public string usuario { get; set; }
+        public DateTime created_ad { get; set; }
     }
 }
