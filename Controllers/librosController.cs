0a5fd6fbc65bf09294d24210c4383d2ff02879d1
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-04-20 10:13:00 -0600

    Subir

--- Controllers/librosController.cs
+++ Controllers/librosController.cs
@@ -1,6 +1,12 @@
 ﻿using L02P2_2021GB650_2021CZ601.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Firebase.Auth;
+using Firebase.Storage;
+using L02P2_2021GB650_2021CZ601.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace L02P2_2021GB650_2021CZ601.Controllers
 {
@@ -31,12 +37,47 @@ namespace L02P2_2021GB650_2021CZ601.Controllers
             ViewData["listadodeequipo"] = listadodeequipo;
             return View();
         }
-        public IActionResult CrearLibros(libros nuevoLibro)
+        public async Task<IActionResult> CrearLibros(libros nuevoLibro, IFormFile archivo)
         {
             _libreriaContext.Add(nuevoLibro);
             _libreriaContext.SaveChanges();
 
+            Stream archivoSubir = archivo.OpenReadStream();
+            string urlImagen = await SubirImagenAFirebase(archivoSubir, archivo.FileName);
+
+            // Establecer la URL de la imagen en el nuevo libro
+            nuevoLibro.url_imagen = urlImagen;
+            _libreriaContext.SaveChanges();
+
+
             return RedirectToAction("Index");
+        }
+
+        public async Task<string> SubirImagenAFirebase(Stream archivoSubir, string nombreArchivo)
+        {
+            string email = "jorgefranciscocz@gmail.com";
+            string clave = "ContraseñaXDXD";
+            string ruta = "desarolloweb-7ffb8.appspot.com";
+            string apikey = "AIzaSyBbIwF8pmsda6lLtldYsro7e_Aa_SCNGq0";
+
+            var auth = new FirebaseAuthProvider(new FirebaseConfig(apikey));
+            var autentificar = await auth.SignInWithEmailAndPasswordAsync(email, clave);
+            var cancellation = new CancellationTokenSource();
+            var tokenuser = autentificar.FirebaseToken;
+
+            var cargararchivo = new FirebaseStorage(ruta,
+                new FirebaseStorageOptions
+                {
+                    AuthTokenAsyncFactory = () => Task.FromResult(tokenuser),
+                    ThrowOnCancel = true
+                }
+            ).Child("Archivos")
+            .Child(nombreArchivo)
+            .PutAsync(archivoSubir, cancellation.Token);
+
+            var urlcarga = await cargararchivo;
+
+            return urlcarga;
         }
 
 
