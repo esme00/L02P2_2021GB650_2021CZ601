96bef9a661f88d9476a3fe110279d27d182169bc
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-04-20 11:22:35 -0600

    MEJORAS

--- Models/libros.cs
+++ Models/libros.cs
@@ -7,12 +7,12 @@ namespace L02P2_2021GB650_2021CZ601.Models
         [Key]
         public int id { get; set; }
         public string nombre { get; set; }
-        public string descripcion {  get; set; }
-        public string url_imagen {  get; set; }
+        public string? descripcion {  get; set; }
+        public string? url_imagen {  get; set; }
         public int id_autor {  get; set; }
         public int id_categoria { get; set; }
         public decimal precio { get; set; }
-        public string estado {  get; set; }
+        public string? estado {  get; set; }
 
     }
 }
