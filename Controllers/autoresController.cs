6fcde13729c1140478697c64bc574891610570f0
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-04-20 09:12:55 -0600

    Context Esme

---
+++ Controllers/autoresController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using L02P2_2021GB650_2021CZ601.Models;
+
+namespace L02P2_2021GB650_2021CZ601.Controllers
+{
+    public class autoresController : Controller
+    {
+        private readonly libreriaContext _context;
+
+        public autoresController(libreriaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: autores
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.autores.ToListAsync());
+        }
+
+        // GET: autores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autores = await _context.autores
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (autores == null)
+            {
+                return NotFound();
+            }
+
+            return View(autores);
+        }
+
+        // GET: autores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: autores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,autor")] autores autores)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(autores);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(autores);
+        }
+
+        // GET: autores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autores = await _context.autores.FindAsync(id);
+            if (autores == null)
+            {
+                return NotFound();
+            }
+            return View(autores);
+        }
+
+        // POST: autores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,autor")] autores autores)
+        {
+            if (id != autores.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(autores);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!autoresExists(autores.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(autores);
+        }
+
+        // GET: autores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autores = await _context.autores
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (autores == null)
+            {
+                return NotFound();
+            }
+
+            return View(autores);
+        }
+
+        // POST: autores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var autores = await _context.autores.FindAsync(id);
+            if (autores != null)
+            {
+                _context.autores.Remove(autores);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool autoresExists(int id)
+        {
+            return _context.autores.Any(e => e.id == id);
+        }
+    }
+}
--- Models/libreriaContext.cs
+++ Models/libreriaContext.cs
@@ -15,9 +15,9 @@ namespace L02P2_2021GB650_2021CZ601.Models
         public DbSet<categorias> categorias { get; set; }
         public DbSet<clientes> clientes { get; set; }
         public DbSet<comentarios_libros> comentarios_libros { get; set; }
-    //    public DbSet<libros> libros { get; set; }  /*  PRINCIPAL*/
-    //    public DbSet<pedido_detalle> pedido_detalle { get; set; }
-    //    public DbSet<pedido_encabezado> pedido_encabezado { get; set; }
-    //
+        public DbSet<libros> libros { get; set; }  /*  PRINCIPAL*/
+        public DbSet<pedido_detalle> pedido_detalle { get; set; }
+        public DbSet<pedido_encabezado> pedido_encabezado { get; set; }
+    
     }
 }
