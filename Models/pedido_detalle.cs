f69f3c6a7bf0cc2ca90f6768f2be5b66d336f2b8
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-04-20 08:55:54 -0600

    Frank

---
+++ Models/pedido_detalle.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P2_2021GB650_2021CZ601.Models
+{
+    public class pedido_detalle
+    {
+        [Key]
+        public int id { get; set; }
+        public int id_pedido { get; set; }
+        public int id_libro { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
---
+++ Models/pedido_encabezado.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P2_2021GB650_2021CZ601.Models
+{
+    public class pedido_encabezado
+    {
+        [Key]
+        public int id { get; set; }
+        public int id_cliente { get; set; }
+        public int cantidad_libros { get; set; }
+        public decimal total { get; set; }
+        public string estado {  get; set; }
+    }
+}
