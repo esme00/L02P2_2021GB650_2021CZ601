ebc7e216bb98f1d9fdd77e4718f22a755ba26795
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-04-20 09:02:05 -0600

    Context

--- Models/libreriaContext.cs
+++ Models/libreriaContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 
 namespace L02P2_2021GB650_2021CZ601.Models
@@ -9,12 +10,13 @@ namespace L02P2_2021GB650_2021CZ601.Models
         {
 
         }
+
+        public DbSet<autores> autores { get; set; }
+        public DbSet<categorias> categorias { get; set; }
+        public DbSet<clientes> clientes { get; set; }
+        public DbSet<comentarios_libros> comentarios_libros { get; set; }
+        public DbSet<libros> libros { get; set; }  /*  PRINCIPAL*/
+        public DbSet<pedido_detalle> pedido_detalle { get; set; }
+        public DbSet<pedido_encabezado> pedido_encabezado { get; set; }
     }
-    public DbSet<autores> autores { get; set; }
-    public DbSet<categorias> categorias { get; set; }
-    public DbSet<clientes> clientes { get; set; }
-    public DbSet<comentarios_libros> comentarios_libros { get; set; }
-    public DbSet<libros> libros { get; set; }  /*  PRINCIPA*/
-    public DbSet<pedido_detalle> pedido_detalle { get; set; }
-    public DbSet<pedido_encabezado> pedido_encabezado { get; set; }
 }
