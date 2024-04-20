441f770870f796cf4a4819c5f7a7a1eb57e069ee
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-04-20 09:51:06 -0600

    eNVVIAR

--- Controllers/librosController.cs
+++ Controllers/librosController.cs
@@ -6,6 +6,7 @@ using Firebase.Storage;
 using L02P2_2021GB650_2021CZ601.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace L02P2_2021GB650_2021CZ601.Controllers
@@ -27,12 +28,12 @@ namespace L02P2_2021GB650_2021CZ601.Controllers
                                    join z in _libreriaContext.categorias on e.id_categoria equals z.id
                                    select new
                                    {
+                                       id = e.id,
                                        nombre = e.nombre,
                                        descripcion = e.descripcion,
                                        precio = e.precio,
                                        autor = m.id,
                                        categoria = z.id
-                                     
                                    }).ToList();
             ViewData["listadodeequipo"] = listadodeequipo;
             return View();
@@ -78,6 +79,45 @@ namespace L02P2_2021GB650_2021CZ601.Controllers
             var urlcarga = await cargararchivo;
 
             return urlcarga;
+        }
+
+   
+        // GET: autores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libros = await _libreriaContext.libros
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (libros == null)
+            {
+                return NotFound();
+            }
+
+            return View(libros);
+        }
+
+        // POST: autores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var libros = await _libreriaContext.libros.FindAsync(id);
+            if (libros != null)
+            {
+                _libreriaContext.libros.Remove(libros);
+            }
+
+            await _libreriaContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool autoresExists(int id)
+        {
+            return _libreriaContext.libros.Any(e => e.id == id);
         }
 
 
